34ac1100f9d8e4ed038eada2e35b9a0d045324fe
Julian Thong <julianzthong@gmail.com>
2021-05-19 17:34:54 -0700

    m

--- server/schema.sql
+++ server/schema.sql
@@ -3,9 +3,21 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID int NOT NULL PRIMARY KEY,
+  messages varchar(255),
+  U_ID int FOREIGN KEY REFERENCES usernames(ID),
+  Room_ID int FOREIGN KEY REFERENCES rooms(ID)
 );
 
+CREATE TABLE usernames (
+  ID int NOT NULL PRIMARY KEY,
+  username varchar(255)
+);
+
+CREATE TABLE rooms (
+  ID int NOT NULL PRIMARY KEY,
+  roomname varchar(255)
+);
 /* Create other tables and define schemas for them here! */
 
 
